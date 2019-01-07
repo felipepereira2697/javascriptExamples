9ac6743cac03dd0423057908475af9b8291ac640
felipepereira <felipepereira2697@gmail.com>
2019-01-06 23:16:00 -0200

    How to delete properties

---
+++ generalJS/deleteProperty.js
@@ -0,0 +1,11 @@
+//At this sample code, it is possible to delete some properties at the object
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};  
+//Now the object myDog doesn't have its tails anymore.
+ delete myDog.tails;
+  
