9f072cf95412e78544d2cb6e6cdbf55878dc8f14
felipepereira <felipepereira2697@gmail.com>
2019-02-19 23:30:41 -0300

    Learning about Symbols in ES6

---
+++ es6/symbols/symbols.js
@@ -0,0 +1,23 @@
+//Symbols is a new datatype in es6 they re unique and immutable data type
+let symbol = Symbol();
+//we can identify the symbol using an string
+let identifiedSymbol = Symbol('identified');
+
+
+
+//we can prove that symbols are unique
+let mySymbol = Symbol("symbol");
+let mySymbol2 = Symbol("symbol");
+
+document.getElementById('div1').innerText = (mySymbol === mySymbol2);
+//document.getElementById('symbolsParagraph').innerText = symbol + " <-OTHER SYMBOL-> "+identifiedSymbol; TYPE ERROR
+//MUST BE EXPLICIT CONVERTED
+document.getElementById('symbolsParagraph').innerText = symbol.toString() + " <-OTHER SYMBOL-> "+identifiedSymbol.toString();
+
+//use case -> Symbols as key properties
+const MY_KEY = Symbol();
+let obj = {};
+obj[MY_KEY] = 14;
+console.log(obj[MY_KEY]);
+
+
