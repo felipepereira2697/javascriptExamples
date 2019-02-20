49d2db68bf103221bc603a011d947c98c109612a
felipepereira <felipepereira2697@gmail.com>
2019-02-20 16:27:46 -0300

    Learning about Symbols and examples

--- es6/symbols/symbols.js
+++ es6/symbols/symbols.js
@@ -20,4 +20,17 @@ let obj = {};
 obj[MY_KEY] = 14;
 console.log(obj[MY_KEY]);
 
+//also you can use to create objects with specific keys
+//REMEMBER: An object key does not need to be a string
+const CONSTANT_KEY = Symbol('definedKey');
+function objectify(value)
+{
+    let obj =  
+    {
+        [CONSTANT_KEY]: value,
+        
+    }
+    return obj;
+}
 
+console.log("OBJECTIFY --> ",objectify("oi, eu sou o Goku!"));
