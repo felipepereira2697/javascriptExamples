de3eb0f452754f96ccc3e6ffca5d14ed010c33bc
felipepereira <felipepereira2697@gmail.com>
2019-02-21 22:43:52 -0300

    Template literals, how to use it and how to work with template literals and functions

---
+++ es6/templateLiteral/templateLiteral.js
@@ -0,0 +1,35 @@
+//will print the white spaces
+console.log(`string line 1
+
+
+
+
+
+
+	string line 2`);
+
+//expression interpolation
+
+let a = 4;
+let b = 10;
+console.log(`Adding ${a} to ${b} is equal to ${a+b}`);
+
+//tagged template literals
+//pass a template literal to a function
+
+function tag(strings, ...values)
+{
+	console.log(strings);
+	console.log(values);
+	console.log(strings.length);
+	console.log(values.length);
+
+
+	return "JS Nuggets";
+}
+
+//pass template literal to a function
+console.log(tag` the correct answer is ${a+b} not ${a*b}`);
+//the example above inputs two arrays --> 1 for the strings and other to template literal values
+// [" the correct answer is ", " not ", ""] values of strings
+// [14, 40] ...values of array
